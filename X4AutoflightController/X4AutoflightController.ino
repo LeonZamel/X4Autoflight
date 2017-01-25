216f580b3922101c3d67606d18c8a44ae81a5388
Fresonty <leon.zamel@gmail.com>
2017-01-25 21:31:18 +0100

    Established connection other direction
    Script can send to Arduino

--- X4AutoflightController/X4AutoflightController.ino
+++ X4AutoflightController/X4AutoflightController.ino
@@ -1,12 +1,14 @@
+int val = 0;
+
 void setup() {
-  // put your setup code here, to run once:
-  // pinMode(9, OUTPUT);
+  pinMode(9, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // analogWrite(9, 255);
-  Serial.write("Hi\n");
-  delay(1000);
+  if (Serial.available()) {
+    val = Serial.parseInt();
+    Serial.println(val, DEC);
+    analogWrite(9, val);
+  }
 }
--- X4AutoflightTracker.py
+++ X4AutoflightTracker.py
@@ -1,8 +1,12 @@
 import serial
+import time
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
-while True :
-    try:
-        state=ser.readline()
-        print(state)
-    except:
-        pass
+val = 0
+while True:
+    if val < 255:
+        val+=1
+    else:
+        val = 0
+    ser.write(bytes([val]))
+    time.sleep(0.1)
