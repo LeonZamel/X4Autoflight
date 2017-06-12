7cb3a3ebba0318305613722b40f2039d67ff9f43
Fresonty <leon.zamel@gmail.com>
2017-06-12 16:00:24 +0200

    Improved color detection

--- X4AutoflightTracker.py
+++ X4AutoflightTracker.py
@@ -8,11 +8,19 @@ from picamera import PiCamera
 # Establish connection to Arduino
 # ser = serial.Serial('/dev/ttyACM0', 9600)
 
+params = cv2.SimpleBlobDetector_Params()
+params.filterByArea = True
+params.minArea = 20
+params.maxArea = 10000
+blobDetector = cv2.SimpleBlobDetector_create(params)
+
+kernel = np.ones((15, 15), np.uint8)
+keyPoints = []
 
 # BGR color boundaries for detecting colors
 colorBoundaries = {
-    "red": ([0, 0, 100], [100, 100, 255]),
-    "blue": ([100, 0, 0], [255, 100, 100])
+    "red": ([80, 80, 180], [220, 240, 255]),
+    "blue": ([180, 60, 60], [255, 230, 190])
 }
 
 # Parts of image capturing code by http://www.pyimagesearch.com
@@ -34,6 +42,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     # analyze image for quad:
     # loop over the boundaries
+    
     for color in colorBoundaries:
         # create NumPy arrays from the boundaries
         lower = np.array(colorBoundaries[color][0], dtype="uint8")
@@ -41,13 +50,38 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     
         # find the colors within the specified boundaries and apply
         # the mask
-        mask = cv2.inRange(image, lower, upper)
-        output = cv2.bitwise_and(image, image, mask=mask)
+        output= cv2.inRange(image, lower, upper)
+
+        output= cv2.medianBlur(output, 11)
+        
+        output = cv2.dilate(output, kernel, iterations = 1)
+        
+        output = cv2.erode(output, kernel, iterations = 2)
+
+        output= cv2.medianBlur(output, 9)
+        
+        cv2.bitwise_not(output, output)
+
+
+        keyPoints.extend(blobDetector.detect(output))
+
         # show the images
         cv2.imshow(color, output)
 
+    
+
+    # pts = np.float([keyPoints[idx].pt for idx in len(keyPoints)]).reshape(-1, 1, 2)
+    print(keyPoints)
+    for point in keyPoints:
+        cv2.circle(image, (int(point.pt[0]), int(point.pt[1])), 10, (0, 255, 0))
+
+    keyPoints = []
+
     # show the frame
     cv2.imshow("Frame", image)
+        
+    
+    # cv2.imshow("Detected", detectedBlobImage)
     key = cv2.waitKey(1) & 0xFF
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
